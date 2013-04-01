2b3ec293c6eb6303a7f1f52ec1a11552736c9aa8
Dan Sindhikara <sindhikara@gmail.com>
2013-04-01 15:44:22 +0900

    Added RDFfromDX.py script

---
+++ bin/RDFfromDX.py
@@ -0,0 +1,9 @@
+import grid.grid as grid
+import sys
+print "# Input format: RDFfromDX.py <dxfilename> <x> <y> <z> <delta> <max>"
+
+dxfilename, x, y, z, delta, maxval = sys.argv[1:]
+griddata = grid.dx2Grid(dxfilename)
+rdf = griddata.interpRDF([float(x),float(y),float(z)], float(delta), float(maxval))
+for i, value in enumerate(rdf):
+    print i*float(delta), value
