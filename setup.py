f9ff8ea97afb17a011070da033bce6a41053ccf6
Daniel J. Sindhikara <sindhikara@gmail.com>
2013-07-29 12:03:35 +0900

    Removed tables requirement

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ config = {
     #'url':'https://github.com/dansind/grid',
     #'download_url': 'https://github.com/dansind/grid/tarball/master',
     'author_email': 'sindhikara@gmail.com',
-    'version': '0.3.0',
-    'install_requires': ['numpy', 'argparse', 'tables'], #
+    'version': '0.3.2',
+    'install_requires': ['numpy', 'argparse', ], #
     #'py_modules': ['grid'], 
     'packages': ["grid","grid.tests"],
     'package_data': {'grid': ['data/shells.json', 'data/points/*', "tests/data/dxfiles/*"], 
